64b583bada11004fc930fb8dc99925b3b790dbe6
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2021-07-28 22:52:07 -0300

    Delete venda directory

--- venda/ItensVenda/CrudItensVendas/class/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define('HOST_Itensvenda','ec2-54-163-97-228.compute-1.amazonaws.com');
-	define('USER_Itensvenda','vhjmuogwqmldtg');
-	define('PASSWORD_Itensvenda','2bbdb436b2188ff48760b1366c2e17d9d8eb93d695f9a83c3d31bd7e7e093eb4');
-	define('BASE_Itensvenda','d9ejdo53669jjr');
-
-?>
